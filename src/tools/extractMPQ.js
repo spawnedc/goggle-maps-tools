69a08fd8f0cdeadea7f68d484ac9093ac8a8097c
Can Yilmaz <can@yilmaz.app>
2025-08-20 13:39:48 +0100

    allow arrays to be passed as file list

--- src/tools/extractMPQ.js
+++ src/tools/extractMPQ.js
@@ -21,16 +21,19 @@ const getPatchFiles = async (dataDir) => {
  * Exports files from MPQ files
  * @param {string} dataDir Wow Data directory where the MPQ files are stored
  * @param {string} dbcFile DBC file name to extract. It shouldn't be an absolute path
- * @param {string} filesToExport Pattern of files to be exported e.g. "DBFilesClient\\*"
+ * @param {string | string[]} filesToExport Pattern of files to be exported e.g. "DBFilesClient\\*"
  * @param {string} exportDir Directory to export the files to
  */
 const extractMPQ = async (dataDir, dbcFile, filesToExport, exportDir) => {
   const patchFiles = await getPatchFiles(dataDir)
+  const exportList = Array.isArray(filesToExport)
+    ? filesToExport
+    : [filesToExport]
   const command = [
     BASE_COMMAND,
     `"${join(dataDir, dbcFile)}"`,
     `-o ${exportDir}`,
-    `-e "${filesToExport}"`,
+    ...exportList.map((f) => `-e "${f}"`),
     ...patchFiles.map((p) => `-p "${p}"`),
   ]
 
